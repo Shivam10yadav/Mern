74d4b3001d1c8d91e9b135aae47bb6500d8aad69
Shivam Yadav <shivam10yadav05@gmail.com>
2025-12-03 14:06:05 +0530

    dashboard name fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -84,27 +84,22 @@ export const loginUser = async (req, res) => {
 
 //controller for getting user by id
 //get:/api/users/data
-export const getUserById=async(req,res)=>{
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
 
-    try {
-        const userId=req.userId
-        //check if user exists
+    const user = await User.findById(userId).select("-password");  // <— FIX
 
-        const user=await User.findById(userId)
-        if(!user){
-            return res.status(404).json({message:'User not found'})
-        }
-        //retrun user
-
-        User.password=undefined;
-        return res.status(200).json({user})
-
-    } catch (error) {
-        return res.status(400).json({message:error.message})
-        
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-}
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
 
 //controller for getting user resume
 //get:/api/users/resumes
